2b3612551009827d71ee16616a947c9995bfe6b8
Lock1515 <landonlockhart15@gmail.com>
2026-06-13 13:36:08 -0500

    frontend: rebuild static assets for merged autonomous changes

--- static/assets/check-BKFSjq69.js
+++
@@ -1,1 +0,0 @@
-import{Xt as e}from"./index-BfiTxdpQ.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ static/assets/square-BfVvfJNb.js
@@ -0,0 +1,1 @@
+import{Xt as e}from"./index-CT6gQE9q.js";var t=e(`square`,[[`rect`,{width:`18`,height:`18`,x:`3`,y:`3`,rx:`2`,key:`afitv7`}]]);export{t};
